fe0a93ebc31370a1ea1436f42d77f976f985b2be
Tungalag1 <tungalag1190@gmail.com>
2020-06-01 18:56:41 +0800

    project2 hiij duuslaa

--- cs142-template-processor.js
+++ cs142-template-processor.js
@@ -1,33 +1,15 @@
-class Cs142TemplateProcessor {
-  constructor(template) {
-    this.template = template;
-  }
-  fillIn(dictionary) {
-    var txt = this.template;
-    var x = txt.match(/{{[^{}]*}}/g);
-    //console.log(x);
-    for (var key in dictionary) {
-      var re = new RegExp("\\{\\{" + key + "\\}\\}");
-      // console.log(re);
-      txt = txt.replace(re, dictionary[key]);
-    }
-    txt = txt.replace(new RegExp("\\{\\{\\w+\\}\\}", "g"), "");
-    console.log(txt);
-    return txt;
-  }
+function Cs142TemplateProcessor(template) {
+  this.template = template;
 }
-
-var template =
-  "My favorite month is {{month}} but not the day {{day}} or the year {{year}}";
-var dateTemplate = new Cs142TemplateProcessor(template);
-
-var dictionary = { month: "July", day: "1", year: "2016" };
-var str = dateTemplate.fillIn(dictionary);
-
-if (str === "My favorite month is July but not the day 1 or the year 2016");
-
-//Case: property doesn't exist in dictionary
-var dictionary2 = { day: "1", year: "2016" };
-var str = dateTemplate.fillIn(dictionary2);
-
-isFinite(str === "My favorite month is  but not the day 1 or the year 2016");
+Cs142TemplateProcessor.prototype.fillIn = function (dictionary) {
+  var str = this.template;
+  var regExp = /{{[^}}]*}}/g;
+  var temp = str.match(regExp); // stringee match hiine tgeed  replace hiine
+  for (var key in dictionary) {
+    var re = new RegExp("\\{\\{" + key + "\\}\\}");
+    str = str.replace(re, dictionary[key]);
+  }
+  str = str.replace(new RegExp("\\{\\{\\w+\\}\\}", "g"), "");
+  console.log(str);
+  return str;
+};
